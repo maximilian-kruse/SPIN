4124f6c95f90e357600bf10afa86586a5ad78908
Max Kruse <maximilian.kruse@kit.edu>
2025-02-02 19:02:00 +0100

    feat: implement pointwise variance computation in LowRankLaplaceApproximation class

--- src/spin/hippylib/laplace.py
+++ src/spin/hippylib/laplace.py
@@ -2,7 +2,6 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Annotated
 
-import dolfin as dl
 import hippylib as hl
 import numpy as np
 import numpy.typing as npt
@@ -20,17 +19,52 @@ class LowRankLaplaceApproximationSettings:
     low_rank_hessian_eigenvectors: Iterable[npt.NDArray[np.floating]]
 
 
+# ==================================================================================================
 class LowRankLaplaceApproximation:
+    # ----------------------------------------------------------------------------------------------
     def __init__(self, settings: LowRankLaplaceApproximationSettings):
+        self._function_space = settings.inference_model.problem.Vh[1]
         self._mean = settings.mean
         low_rank_hessian_eigenvectors = fex_converter.convert_to_multivector(
-            settings.low_rank_hessian_eigenvectors, settings.inference_model.problem.Vh[1]
+            settings.low_rank_hessian_eigenvectors, self._function_space
         )
+        mean_vector = fex_converter.convert_to_dolfin(self._mean, self._function_space)
         self._laplace_approximation = hl.GaussianLRPosterior(
             settings.inference_model.prior,
             settings.low_rank_hessian_eigenvalues,
             low_rank_hessian_eigenvectors,
+            mean_vector,
         )
 
-    def compute_pointwise_variance(self) -> npt.NDArray[np.floating]:
-        pass
+    # ----------------------------------------------------------------------------------------------
+    def compute_pointwise_variance(
+        self,
+        method: Annotated[str, Is[lambda x: x in ("Exact", "Estimator", "Randomized")]],
+        num_expansion_values_estimator: Annotated[int, Is[lambda x: x > 0]] | None = None,
+        num_eigenvalues_randomized: Annotated[int, Is[lambda x: x > 0]] | None = None,
+    ) -> npt.NDArray[np.floating]:
+        if method == "Exact":
+            variance = self._laplace_approximation.pointwise_variance()
+        if method == "Estimator":
+            if num_expansion_values_estimator is None:
+                raise ValueError(
+                    "num_expansion_values_estimator must be provided for 'Estimator' method."
+                )
+            variance = self._laplace_approximation.pointwise_variance(
+                method=method, k=num_expansion_values_estimator
+            )
+        if method == "Randomized":
+            if num_eigenvalues_randomized is None:
+                raise ValueError(
+                    "num_eigenvalues_randomized must be provided for 'Randomized' method."
+                )
+            variance, *_ = self._laplace_approximation.pointwise_variance(
+                method=method, r=num_eigenvalues_randomized
+            )
+        pointwise_variance = fex_converter.convert_to_numpy(variance, self._function_space)
+        return pointwise_variance
+
+    # ----------------------------------------------------------------------------------------------
+    @property
+    def hippylib_gaussian_posterior(self) -> hl.GaussianLRPosterior:
+        return self._laplace_approximation
